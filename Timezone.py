47e8526d54429a705a29d6c5a0d94a6b0e471542
Folk <Tanawut.pf@gmail.com>
2024-12-08 20:51:29 +0700

    Final-_IFNAME

--- Timezone.py
+++ Timezone.py
@@ -48,3 +48,6 @@ def convert_time_to_thailand(country_name, timezone):
         print(f"The time in Thailand is: {thailand_time.strftime('%H:%M')}")
     except ValueError:
         print("Invalid time format. Please enter the time in HH:MM format.")
+
+if __name__ == "__main__":
+    main()
